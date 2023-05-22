3bd86b164b561cb29c27e0af897adbd6137cc69b
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2023-05-22 20:54:59 +0700

    class manusia

--- MainJM/manusia.h
+++ MainJM/manusia.h
@@ -1,1 +1,19 @@
-#pragma once
+#ifdef MANUSIA_H
+#define MANUSIA_H
+using namespace std;
+
+class manusia {
+public:
+	string nama;
+	jantung varJantung;
+
+	manusia(string pNama)
+		: nama(pNama) {
+		cout << nama << "hidup\n";
+
+	}
+	~manusia() {
+		cout << nama << "mati\n";
+	}
+};
+#endif 
