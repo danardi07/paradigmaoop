9b1f955cae5cc4ed58426ea846744689df332669
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2023-05-22 20:55:51 +0700

    class mahasiswa

--- paradigmaoop/paradigmaoop.cpp
+++ paradigmaoop/paradigmaoop.cpp
@@ -6,7 +6,7 @@ class mahasiswa {
 private:
 	const int id;
 	string nama;
-	float nilai;
+	float nilai; 
 
 public:
 	mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai(pNilai) { //member
